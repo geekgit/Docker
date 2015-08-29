47833144e422316249f71f57b53f0251f91b3f86
geekgit <geeekz@gmail.com>
2015-08-29 23:06:57 +0500

    fix HOME path

--- firefox_docker/firefox-docker.sh
+++ firefox_docker/firefox-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 xhost local:root
-docker run -ti --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/vivid/Docker/Firefox:/home/user firefox_docker "$@"
+docker run -ti --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $HOME/Docker/Firefox:/home/user firefox_docker "$@"
