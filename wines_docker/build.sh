cfb05e5fb3e9a88bffe64aa9a7144c38e2c96a77
geekgit <geeekz@gmail.com>
2015-09-02 23:27:08 +0500

    Wine Staging Docker

---
+++ wines_docker/build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker build -t wine_docker ~/Git/wines_docker
---
+++ wines_docker/wine-docker.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run -ti --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $HOME/Docker/Wine:/home/user/share wine_docker "$@"
