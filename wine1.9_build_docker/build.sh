30c9de0c626630f4a232fe354832140494b2238e
geekgit <geeekz@gmail.com>
2016-01-01 13:00:03 +0500

    wine 1.9 build dockerfile

---
+++ wine1.9_build_docker/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+ContainerName="wine1.9_build_docker"
+ArchiveName="wine1.9.0.tar.gz"
+echo "building image..."
+docker build -t $ContainerName ~/$ContainerName
+echo "starting container..."
+ID=$(docker create $ContainerName)
+echo "container id: $ID"
+echo "docker copying files..."
+mkdir $HOME/$ContainerName-share
+docker cp $ID:/home/build/buffer.tar.gz $HOME/$ContainerName-share/$ArchiveName
+cd "$HOME/$ContainerName-share"
+echo "untar..."
+tar -xvf "$ArchiveName"
+echo "deleting tar archive"
+rm "$ArchiveName"
+echo "docker removing container..."
+docker rm -v $ID
+echo "container removed."
---
+++ wine1.9_build_docker/update-wine1.9-links.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+PathToOpt="/opt/wine-1.9.0/bin"
+OriginalPath="/usr/bin"
+Filenames="msidb msiexec notepad regedit regsvr32 dil wine wineboot winebuild winecfg wineconsole winecpp winedb winedump winefile wineg++ winegcc winemaker winemine winepath wine-preloader wineserver wmc wrc"
+arr=($Filenames)
+
+for f in "${arr[@]}"
+do
+	Src="$OriginalPath/$f"
+	Dest="$PathToOpt/$f"
+	echo "$Src <- $Dest"
+	ln -sf "$Dest" "$Src"
+done
