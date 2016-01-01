4e436d266d01fb0e770397ac371e25d583436815
geekgit <geeekz@gmail.com>
2016-01-02 00:18:13 +0500

    xqemu

---
+++ xqemu_build_docker/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+ContainerName="xqemu_build_docker"
+DebName="xqemu-git.deb"
+echo "building image..."
+docker build -t $ContainerName ~/$ContainerName
+echo "starting container..."
+ID=$(docker create $ContainerName)
+echo "container id: $ID"
+echo "docker copying files..."
+mkdir $HOME/$ContainerName-share
+docker cp $ID:/root/$DebName $HOME/$ContainerName-share/$DebName
+echo "docker removing container..."
+docker rm -v $ID
+echo "container removed."
