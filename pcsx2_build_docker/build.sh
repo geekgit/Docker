ba6a18a3772fbe3b6bd44cdd2bee6b9547a64c39
geekgit <geeekz@gmail.com>
2016-01-02 19:11:18 +0500

    update

---
+++ pcsx2_build_docker/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+ContainerName="pcsx2_build_docker"
+TarGzName="pcsx2-git.tar.gz"
+echo "building image..."
+docker build -t $ContainerName ~/$ContainerName
+echo "starting container..."
+ID=$(docker create $ContainerName)
+echo "container id: $ID"
+echo "docker copying files..."
+mkdir $HOME/$ContainerName-share
+docker cp $ID:/home/build/pcsx2/$TarGzName $HOME/$ContainerName-share/$TarGzName
+echo "docker removing container..."
+docker rm -v $ID
+echo "container removed."
