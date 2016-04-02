5c5b9d3a4074f315bdef9c821ecd1f859129f26b
geekgit <geeekz@gmail.com>
2016-04-02 15:51:12 +0000

    nginx_ubuntu_build_docker

---
+++ nginx_ubuntu_build_docker/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+CurrPath=$(pwd)
+echo "Path: $CurrPath"
+Basename=${CurrPath##*/}
+echo "Base name: $Basename"
+ContainerName="$Basename"
+echo "Container name: $ContainerName"
+echo "building image..."
+echo "docker build -t $Basename $CurrPath"
+docker build -t $ContainerName $CurrPath
+echo "starting container..."
+ID=$(docker create $ContainerName)
+echo "docker copying files..."
+mkdir $HOME/$ContainerName-share
+docker cp $ID:/home/build/gpg-verify.log $HOME/$ContainerName-share/gpg-verify.log
+docker cp $ID:/home/build/configure.log $HOME/$ContainerName-share/configure.log
+docker cp $ID:/root/nginx-opt-ubuntu.deb $HOME/$ContainerName-share/nginx-opt-ubuntu.deb 
+echo "docker removing container..."
+docker rm -v $ID
+echo "container removed."
