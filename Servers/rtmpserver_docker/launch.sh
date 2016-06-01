deeca139bb5841bc49b23f8b7728f4c46125d495
geekgit <geeekz@gmail.com>
2016-06-01 14:02:00 +0000

    tmpfs

--- Servers/rtmpserver_docker/launch.sh
+++ Servers/rtmpserver_docker/launch.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+# NOTE: tmpfs -> Docker 1.10+
 CurrPath=$(pwd)
 echo "Path: $CurrPath"
 Basename=${CurrPath##*/}
 echo "Name: $Basename"
 echo "docker run -ti --rm $Basename"
-docker run -ti --rm -p 1935:1935 -p 8080:80 $Basename
+docker run -ti --rm --tmpfs /tmp -p 1935:1935 -p 8080:80 $Basename
