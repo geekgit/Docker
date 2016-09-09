ea4e14925954fb86f1d876581690e1d200f05195
geekgit <geeekz@gmail.com>
2016-09-09 23:11:25 +0000

    wine 1.9.18

--- Build/wine1.9_build_docker/build.sh
+++ Build/wine1.9_build_docker/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ContainerName="wine1.9_build_docker"
-ArchiveName="wine1.9.17.tar.gz"
+ArchiveName="wine1.9.18.tar.gz"
 echo "building image..."
 docker build -t $ContainerName $(pwd)
 echo "starting container..."
