30747aaf5c061510e67cc14abe5736b726e33358
geekgit <geeekz@gmail.com>
2016-09-09 23:13:10 +0000

    wine 1.8.4

--- Build/wine1.8_build_docker/update-wine1.8-links.sh
+++ Build/wine1.8_build_docker/update-wine1.8-links.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PathToOpt="/opt/wine-1.8.3/bin"
+PathToOpt="/opt/wine-1.8.4/bin"
 OriginalPath="/usr/bin"
 Filenames="msidb msiexec notepad regedit regsvr32 dil wine wineboot winebuild winecfg wineconsole winecpp winedb winedump winefile wineg++ winegcc winemaker winemine winepath wine-preloader wineserver wmc wrc"
 arr=($Filenames)
