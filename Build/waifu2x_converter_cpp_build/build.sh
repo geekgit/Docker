c55efa9a2bb32dfbbdbbcaf43307cf43e41576e5
geekgit <geeekz@gmail.com>
2017-06-13 13:33:14 +0000

    waifu2x_converter_cpp_build

---
+++ Build/waifu2x_converter_cpp_build/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+CurrPath=$(pwd)
+echo "Path: $CurrPath"
+Basename=${CurrPath##*/}
+echo "Base name: $Basename"
+ContainerName="$Basename"
+echo "Container name: $ContainerName"
+echo "building image..."
+echo "docker build -t $Basename $CurrPath"
+docker build -t $ContainerName $CurrPath
+echo "starting container..."
+ID=$(docker create $ContainerName)
+echo "docker copying files..."
+mkdir $HOME/$ContainerName-share
+docker cp $ID:/home/build/waifu2x-converter-cpp/waifu2x-converter-cpp $HOME/$ContainerName-share/waifu2x-converter-cpp
+echo "md5"
+cd "$HOME/$ContainerName-share"
+geekgit-md5-calculate *
+echo "return to old pwd..."
+cd "$CurrPath"
+echo "docker removing container..."
+docker rm -v $ID
+echo "container removed."
