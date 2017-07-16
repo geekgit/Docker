05aa888ac0d7150aa827feab9c265582434e369f
geekgit <geeekz@gmail.com>
2017-07-16 15:41:28 +0000

    update build.sh

--- Build/waifu2x_converter_cpp_build/build.sh
+++ Build/waifu2x_converter_cpp_build/build.sh
@@ -13,9 +13,13 @@ ID=$(docker create $ContainerName)
 echo "docker copying files..."
 mkdir $HOME/$ContainerName-share
 docker cp $ID:/home/build/waifu2x-converter-cpp/waifu2x-converter-cpp $HOME/$ContainerName-share/waifu2x-converter-cpp
+docker cp $ID:/home/build/waifu2x-converter-cpp/opencv-install/lib/libopencv_imgcodecs.so.3.0.0 $HOME/$ContainerName-share/libopencv_imgcodecs.so.3.0
+docker cp $ID:/home/build/waifu2x-converter-cpp/opencv-install/lib/libopencv_imgproc.so.3.0.0 $HOME/$ContainerName-share/libopencv_imgproc.so.3.0
+docker cp $ID:/home/build/waifu2x-converter-cpp/opencv-install/lib/libopencv_core.so.3.0.0 $HOME/$ContainerName-share/libopencv_core.so.3.0
 echo "md5"
 cd "$HOME/$ContainerName-share"
-geekgit-md5-calculate *
+geekgit-md5-calculate waifu2x-converter-cpp
+geekgit-md5-calculate *.so.*
 echo "return to old pwd..."
 cd "$CurrPath"
 echo "docker removing container..."
