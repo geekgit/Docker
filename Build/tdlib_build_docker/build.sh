dc606de087de92123642ae394040936a688d87d7
geekgit <geeekz@gmail.com>
2018-05-10 17:47:37 +0000

    tdlib_build_docker

---
+++ Build/tdlib_build_docker/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+ContainerName="tdlib_build_docker"
+echo "building image..."
+docker build -t $ContainerName $(pwd)
+echo "starting container..."
+ID=$(docker create $ContainerName)
+echo "container id: $ID"
+echo "docker copying files..."
+mkdir $HOME/$ContainerName-share
+#docker cp $ID:/home/build/tdlib.tar.gz $HOME/$ContainerName-share/tdlib.tar.gz
+# or
+#docker cp $ID:/home/build/tdlib.7z $HOME/$ContainerName-share/tdlib.7z
+echo "docker removing container..."
+docker rm -v $ID
+echo "container removed."
