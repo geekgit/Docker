b617afd1387fcea5b80707508a422d01af79e50f
geekgit <geeekz@gmail.com>
2018-10-11 06:18:15 +0000

    ffmpeg4_ubuntu_build_docker

---
+++ Build/ffmpeg4_ubuntu_build_docker/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+CurrPath=$(pwd)
+echo "Path: $CurrPath"
+Basename=${CurrPath##*/}
+echo "Base name: $Basename"
+ContainerName="$Basename"
+echo "Container name: $ContainerName"
+echo "building image..."
+echo "docker build -t $Basename $CurrPath"
+docker build -t $ContainerName $CurrPath
+echo "starting container..."
+ID=$(docker create $ContainerName)
+echo "docker copying files..."
+mkdir $HOME/$ContainerName-share
+docker cp $ID:/home/build/ffmpeg4.0.2-opt.deb $HOME/$ContainerName-share/ffmpeg4.0.2-opt.deb
+docker cp $ID:/home/build/gpg-check-log.txt $HOME/$ContainerName-share/gpg-check-log.txt
+docker cp $ID:/home/build/configure-log.txt $HOME/$ContainerName-share/configure-log.txt
+docker cp $ID:/home/build/ffmpeg-file-log.txt $HOME/$ContainerName-share/ffmpeg-file-log.txt
+echo "docker removing container..."
+docker rm -v $ID
+echo "container removed."
