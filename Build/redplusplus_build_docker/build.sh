a781ab448b3f819856fdaba7a8ed2e2a50a3bed3
geekgit <geeekz@gmail.com>
2018-10-11 08:04:11 +0000

    redplusplus_build_docker

---
+++ Build/redplusplus_build_docker/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+CurrPath=$(pwd)
+echo "Path: $CurrPath"
+Basename=${CurrPath##*/}
+echo "Base name: $Basename"
+ContainerName="$Basename"
+echo "Container name: $ContainerName"
+echo "building image..."
+echo "docker build -t $Basename $CurrPath"
+docker build -t $ContainerName $CurrPath
+echo "starting container..."
+ID=$(docker create $ContainerName)
+echo "docker copying files..."
+mkdir $HOME/$ContainerName-share
+docker cp $ID:/home/build/RedPlusPlus/redplusplus-3.0.0-beta.gbc $HOME/$ContainerName-share/redplusplus-3.0.0-beta.gbc
+echo "go and checksum check"
+cd "$HOME/$ContainerName-share"
+geekgit-md5-calculate *.gbc
+echo "return to old pwd..."
+cd "$CurrPath"
+echo "docker removing container..."
+docker rm -v $ID
+echo "container removed."
