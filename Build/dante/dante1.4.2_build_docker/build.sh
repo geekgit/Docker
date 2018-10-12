e63f37f33fe5dfc7e2b7306951bfdb0fe66129b8
geekgit <geeekz@gmail.com>
2018-10-12 06:41:35 +0000

    dante1.4.2_build_docker

---
+++ Build/dante/dante1.4.2_build_docker/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+CurrPath=$(pwd)
+echo "Path: $CurrPath"
+Basename=${CurrPath##*/}
+echo "Base name: $Basename"
+ContainerName="$Basename"
+echo "Container name: $ContainerName"
+echo "building image..."
+echo "docker build -t $Basename $CurrPath"
+docker build -t $ContainerName $CurrPath
+echo "starting container..."
+ID=$(docker create $ContainerName)
+echo "docker copying files..."
+mkdir $HOME/$ContainerName-share
+docker cp $ID:/home/build/dante-server1.4.2-opt.deb $HOME/$ContainerName-share/dante-server1.4.2-opt.deb
+echo "docker removing container..."
+docker rm -v $ID
+echo "container removed."
