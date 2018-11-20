bc2cd32890823e3a0549737a4d374805e849f7d6
geekgit <geeekz@gmail.com>
2018-11-20 11:32:46 +0000

    waifu2x_converter_cpp_build

---
+++ Build/waifu2x/waifu2x_converter_cpp_build/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+CurrPath=$(pwd)
+echo "Path: $CurrPath"
+Basename=${CurrPath##*/}
+echo "Base name: $Basename"
+ContainerName="$Basename"
+echo "Container name: $ContainerName"
+echo "building image..."
+echo "docker build -t $Basename $CurrPath"
+docker build -t $ContainerName $CurrPath
+echo "starting container..."
+ID=$(docker create $ContainerName)
+echo "docker copying files..."
+mkdir $HOME/$ContainerName-share
+docker cp $ID:/home/build/waifu2x-cpp-opt.deb $HOME/$ContainerName-share/waifu2x-cpp-opt.deb
+echo "docker removing container..."
+docker rm -v $ID
+echo "container removed."
