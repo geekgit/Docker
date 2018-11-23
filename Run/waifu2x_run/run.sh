97e8e1a0edb5f3a8343fe83bd0b95f0127abb59d
geekgit <geeekz@gmail.com>
2018-11-23 09:33:39 +0000

    waifu2x_run

---
+++ Run/waifu2x_run/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+CurrPath=$(pwd)
+echo "Path: $CurrPath"
+Basename=${CurrPath##*/}
+echo "Base name: $Basename"
+ContainerName="$Basename"
+echo "Container name: $ContainerName"
+echo "building image..."
+echo "docker build -t $Basename $CurrPath"
+docker build -t $ContainerName $CurrPath
+echo "starting container..."
+ID=$(docker create $ContainerName)
+echo "docker copying files..."
+mkdir $HOME/$ContainerName-share
+docker cp $ID:/home/waifu/output.7z $HOME/$ContainerName-share/output.7z
+echo "docker removing container..."
+docker rm -v $ID
+echo "container removed."
