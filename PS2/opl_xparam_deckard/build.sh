2f38e9dd52c4824d0fe4d587e99a2f31a4626709
geekgit <geeekz@gmail.com>
2023-05-21 04:30:51 +0000

    opl_xparam_deckard

---
+++ PS2/opl_xparam_deckard/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+CurrPath=$(pwd)
+echo "Path: $CurrPath"
+Basename=${CurrPath##*/}
+echo "Base name: $Basename"
+ContainerName="$Basename"
+echo "Container name: $ContainerName"
+echo "building image..."
+echo "docker build -t $Basename $CurrPath"
+docker build -t $ContainerName $CurrPath
+echo "starting container..."
+ID=$(docker create $ContainerName)
+echo "ID: ${ID}"
+echo "docker copying files..."
+mkdir $HOME/$ContainerName-share
+docker cp $ID:/home/build/Open-PS2-Loader/opl_stripped.elf $HOME/$ContainerName-share/opl_xparam_deckard_stripped.elf
+docker cp $ID:/home/build/Open-PS2-Loader/opl.elf $HOME/$ContainerName-share/opl_xparam_deckard.elf
+echo "docker removing container..."
+docker rm -v $ID
+echo "container removed."
